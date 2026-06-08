94d955aafad068b9c271b1bd0744a41ed8a88db3
Lucas-de-Lara-Biasi <lucas.biasi@escola.pr.gov.br>
2026-06-08 06:36:15 -0300

    uploaded > primeira versão do site
    ainda faltam bastante coisa mais eu faço.

---
+++ main.js
@@ -0,0 +1,25 @@
+const theme_button = document.getElementById("theme_checkbox")
+var THEME = "light"
+
+//Carrega do localStorage o valor do tema (caso ja tenha algo salvo) >>
+if (localStorage.getItem("theme")) {
+    THEME = localStorage.getItem("theme")
+    document.documentElement.setAttribute('data-theme', THEME);
+
+    if (THEME == "dark") {
+        theme_button.setAttribute("checked", true)
+    }
+}
+
+//Add um listener para o checkbox que muda o tema >>
+theme_button.addEventListener("change", () => {
+    //SE FOR CLARO MUDA PRO MODO ESCURO E VISSE VERSA
+    if (theme_button.checked) {
+        THEME = "dark"
+    } else {
+        THEME = "light"
+    }
+
+    document.documentElement.setAttribute('data-theme', THEME);
+    localStorage.setItem("theme", THEME)
+})
